41fa951f054b432638a98fc1883f4afeca731ca7
BaloghMartin <baloghmartin@icloud.com>
2023-11-02 17:52:06 +0100

    firstfit alakul

--- src/com/company/FirstFit.java
+++ src/com/company/FirstFit.java
@@ -3,66 +3,87 @@ package com.company;
 import java.util.List;
 
 public class FirstFit {
-    private static int[][] finder(int[][] solution, int meret, int N, int M, int hivnum, List<Integer> guideSequence) throws Except.Tulkicsi {
+    public static int[][] placeSquares(int meret, List<Integer> guideSequence) throws Except.Tulkicsi {
         int currentGuideIndex = 0;
-        int n = N;
-        int m = M;
 
-        while (currentGuideIndex < guideSequence.size()) {
-            int currentGuide = guideSequence.get(hivnum - 1);
-
-            if (currentGuide == 1) {
-                int sor = 0;
-                while (sor + meret <= solution.length - 1) {
-                    n = sor;
-                    m = 0;
-                    while (n + meret <= solution.length) {
-                        int[][] result = finderHelper(solution, meret, n, m);
-                        if (result != null) {
-                            return result;
-                        }
-                        n++;
-                    }
-                    sor++;
-                }
+        int hivnum = 1;
+        double opt = 0;
+        int optSeg = meret;
+        while (true) {
+            opt = opt + (optSeg * optSeg);
+            optSeg--;
+            if (optSeg == 0) {
+                break;
             }
-
-            throw new Except.Tulkicsi("nem jo a sorba");
         }
+        double optV = Math.sqrt(opt);
+        int optV2 = (int) Math.round(optV);
+        int[][] solution;
 
+        while(true){
+            solution = new int[optV2+hivnum-1][optV2+hivnum-1];
+            while(true) {
+                while (currentGuideIndex < guideSequence.size()) {
+                    int currentGuide = guideSequence.get(hivnum - 1);
+
+                if (currentGuide == 1) {
+                    int[] position = finder(solution, meret);
+                    if (position != null) {
+                        placer(solution, meret, position[0], position[1]);
+                    } else {
+                        throw new Except.Tulkicsi("nem fér bele");
+                    }
+                }
+
+                currentGuideIndex++;
+            }
+            hivnum++;
+            if(hivnum==meret){
+                break;
+            }
+        }
+            break;
+        }
         return solution;
     }
 
-    private static int[][] finderHelper(int[][] solution, int meret, int N, int M) {
-        int n = N;
-        int m = M;
+    private static int[] finder(int[][] solution, int meret) {
+        int n = 0;
+        int m = 0;
 
-        while (true) {
-            if (isAvailable(solution, n, m, meret)) {
-                return writer(solution, n, m, meret);
-            } else {
-                m += 1;
-            }
-        }
-    }
-
-    private static boolean isAvailable(int[][] solution, int n, int m, int meret) {
-        for (int i = n; i < n + meret; i++) {
-            for (int j = m; j < m + meret; j++) {
-                if (i >= solution.length || j >= solution[0].length || solution[i][j] != 0) {
-                    return false;
+        int sor = 0;
+        while (sor + meret <= solution.length - 1) {
+            n = sor;
+            m = 0;
+            while (n + meret <= solution.length) {
+                boolean canPlace = true;
+                for (int i = n; i < n + meret; i++) {
+                    for (int j = m; j < m + meret; j++) {
+                        if (i >= solution.length || j >= solution[0].length || solution[i][j] != 0) {
+                            canPlace = false;
+                            break;
+                        }
+                    }
+                    if (!canPlace) {
+                        break;
+                    }
                 }
+                if (canPlace) {
+                    return new int[]{n, m};
+                }
+                n++;
             }
+            sor++;
         }
-        return true;
+
+        return null;
     }
 
-    private static int[][] writer(int[][] solution, int n, int m, int meret) {
+    private static void placer(int[][] solution, int meret, int n, int m) {
         for (int i = n; i < n + meret; i++) {
             for (int j = m; j < m + meret; j++) {
                 solution[i][j] = meret;
             }
         }
-        return solution;
     }
 }
