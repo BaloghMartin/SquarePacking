cc87860a24ec91076efdf7172377f584d9a362fd
BaloghMartin <baloghmartin@icloud.com>
2023-11-02 19:31:40 +0100

    nem jó mert csak egymás mellé teszi az elemeket és abból hagy ki egy oszlopot a végén

--- src/com/company/FirstFit.java
+++ src/com/company/FirstFit.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class FirstFit {
     public static int[][] placeSquares(int maxMeret, List<Integer> guideSequence) throws Except.Tulkicsi {
         int currentGuideIndex = 0;
-        int meretHelp=maxMeret;
-        int hivnum = 0;
+        int meretHelp = maxMeret;
+        int hivnum = 1;
         double opt = 0;
         int optSeg = maxMeret;
         while (true) {
@@ -20,33 +20,39 @@ public class FirstFit {
         int optV2 = (int) Math.round(optV);
         int[][] solution;
 
-        while(true){
+        while (true) {
+            //System.out.println("Entering the outer loop");
             //tömblétrehozás
-            meretHelp=maxMeret;
-            solution = new int[optV2+hivnum-1][optV2+hivnum-1];
+            meretHelp = maxMeret;
+            solution = new int[optV2 + hivnum - 1][optV2 + hivnum - 1];
 
-                //elemetetés
-                while (currentGuideIndex < guideSequence.size()) {
-                    //elbánás a tömbbel case-ek szerint
-                    int currentGuide = guideSequence.get(hivnum - 1);
-                    //helykeresés
-                    int[] position = finder(solution, meretHelp);
-                    if (position != null) {
-                        //ha van hely beírja
-                        solution = placer(solution, meretHelp, position[0], position[1]);
-                        currentGuideIndex++;
-                        meretHelp--;
-                        if (meretHelp==0){return solution;}
-                    } else {
-                        //ha nem akkor ugrik és növeli a méretet
-                        break;
+            //elemetetés
+            while (currentGuideIndex < guideSequence.size()) {
+                System.out.println("Entering the inner loop");
+
+                //elbánás a tömbbel case-ek szerint
+                int currentGuide = guideSequence.get(currentGuideIndex); // Change this line
+                //helykeresés
+                int[] position = finder(solution, meretHelp);
+                if (position != null) {
+                    //ha van hely beírja
+                    solution = placer(solution, meretHelp, position[0], position[1]);
+                    System.out.println("Placed square of size " + meretHelp + " at position (" + position[0] + ", " + position[1] + ")");
+                    currentGuideIndex++;
+                    meretHelp--;
+                    if (meretHelp == 0) {
+                        return solution;
                     }
+                } else {
+                    //ha nem akkor ugrik és növeli a méretet
+                    System.out.println("No suitable place found. Increasing size.");
+                    break;
                 }
-                hivnum++;
+            }
 
-
+            currentGuideIndex=0;
+            hivnum++;
         }
-
     }
 
     private static int[] finder(int[][] solution, int meret) {
@@ -54,62 +60,66 @@ public class FirstFit {
         int m = 0;
         int[] place;
         int sor = 0;
-        /*while (sor + meret <= solution.length - 1) {
-            n = sor;
-            m = 0;
-            while (n + meret <= solution.length) {
-                boolean canPlace = true;
-                for (int i = n; i < n + meret; i++) {
-                    for (int j = m; j < m + meret; j++) {
-                        if (i >= solution.length || j >= solution[0].length || solution[i][j] != 0) {
-                            canPlace = false;
-                            break;
-                        }
-                    }
-                    if (!canPlace) {
+
+        while (true) {
+            System.out.println("Entering the outer loop");
+            //sorléptetés
+            while (true) {
+                System.out.println("Entering the row loop");
+
+                //oszlopléptetés
+                //vizsgált hely nem jó
+                if (solution[n][m] != 0) {
+                    System.out.println("Cell not empty: (" + n + ", " + m + ")");
+                    m += solution[n][m];
+                    if (m + meret > solution.length) {
+                        System.out.println("m + meret >= solution.length");
                         break;
                     }
                 }
-                if (canPlace) {
-                    return new int[]{n, m};
-                }
-                n++;
-            }
-            sor++;
-        }
-*/
-        while(true){
-            //sorléptetés
-            while(true){
-                //oszlopléptetés
-                //vizsgált hely nem jó
-                if (solution[n][m]!=0){
-                    m+=solution[n][m];
-                }
-                if (solution[n][m]==0){
-                    //a többi négy sarok nem jó
-                    if (solution[n+meret][m+meret]!=0 || solution[n][m+meret]!=0 || solution[n+meret][m]!=0){
-                        m++;
-                    }
-                    //jó a hely
-                    else return place= new int[]{n, m};
-                }
-                if (m>=solution.length){break;}
 
+                if (solution[n][m] == 0) {
+                    System.out.println("Cell is empty: (" + n + ", " + m + ")");
+                    //a többi négy sarok nem jó
+                    if (m + meret >= solution.length) {
+                        System.out.println("m + meret >= solution.length");
+                        break;
+                    }
+                    if (solution[n + meret][m + meret] != 0 || solution[n][m + meret] != 0 || solution[n + meret][m] != 0) {
+                        System.out.println("Adjacent cells not empty");
+                        m++;
+                        if (m + meret > solution.length) {
+                            System.out.println("m + meret >= solution.length");
+                            break;
+                        }
+                    }
+
+                    //jó a hely
+                    else {
+                        System.out.println("Good place found: (" + n + ", " + m + ")");
+                        return place = new int[]{n, m};
+                    }
+                }
             }
             n++;
-            if (n>= solution.length){break;}
+            if (n + meret > solution.length) {
+                System.out.println("n + meret >= solution.length");
+                break;
+            }
         }
 
+        System.out.println("Exiting the finder function");
         return null;
     }
+
 
     private static int[][] placer(int[][] solution, int meret, int n, int m) {
         for (int i = n; i < n + meret; i++) {
             for (int j = m; j < m + meret; j++) {
                 solution[i][j] = meret;
+                System.out.println("Placing square of size " + meret + " at position (" + i + ", " + j + ")");
             }
         }
+        System.out.println("Exiting the placer function");
         return solution;
-    }
-}
+    }}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code hereasdaaaa
+        int maxMeret = 5;
+        List<Integer> guideSequence = new ArrayList<>();
+        guideSequence.add(1);
+        guideSequence.add(1);
+        guideSequence.add(1);
+        guideSequence.add(1);
+        guideSequence.add(1);
+
+        try {
+            int[][] result = FirstFit.placeSquares(maxMeret, guideSequence);
+
+            // Print the result to check the placement of squares
+            for (int[] row : result) {
+                for (int value : row) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+        } catch (Except.Tulkicsi e) {
+            e.printStackTrace();
+        }
     }
 }
